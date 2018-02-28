f93f22611816ce4412db6c159c4569a189b4684d
Chen Weiguang <chen.weiguang@gmail.com>
2018-02-28 22:27:06 +0800

    Add new method to Logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+extern crate fruently;
+#[macro_use]
+extern crate log;
+
+use fruently::fluent::Fluent;
+use std::borrow::Cow;
+use std::net::ToSocketAddrs;
+
+pub struct Logger<'a, A>
+where
+    A: ToSocketAddrs,
+{
+    fluent: Fluent<'a, A>,
+}
+
+impl<'a, A> Logger<'a, A>
+where
+    A: ToSocketAddrs,
+{
+    pub fn new<T>(addr: A, tag: T) -> Logger<'a, A>
+    where
+        T: Into<Cow<'a, str>>,
+    {
+        Logger {
+            fluent: Fluent::new(addr, tag),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_new() {
+        let _ = Logger::new("127.0.0.1:24224", "test-index");
     }
 }
